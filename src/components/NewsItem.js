8f78743704db91f0b75e33c77a4011529148e904
mujassimtaj <mujassimtaj17@gmail.com>
2022-11-16 13:00:14 +0530

    some changes

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
+import News from './news';
 
 class NewsItem extends Component {
+
     render() {
-        let { title, description } = this.props
+        let { title, description , imageUrl, newsUrl} = this.props
         return (
             <div>
                 <div className="card" style={{width: "18rem"}}>
-                    <img src="..." className="card-img-top" alt="..." />
+                    <img src={!imageUrl ? "https://i2-prod.manchestereveningnews.co.uk/incoming/article24308693.ece/ALTERNATES/s615/2_bbcnewsJPG.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}...</p>
+                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">read more</a>
                     </div>
                 </div>
             </div>
--- src/components/news.js
+++ src/components/news.js
@@ -1,24 +1,40 @@
-import React, { Component } from 'react';
-import NewsItem from './NewsItem';
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export default class News extends Component {
-    render() {
-        return (
-            <div className='container my-3'>
-                <h2>NewsMonkey-Top Headlines </h2>
-                <div className="row">
-                <div className="col-md-4">                     
-                <NewsItem title= " my title" description = "my desc" />
-                </div>
-                <div className="col-md-4">                     
-                <NewsItem title= " my title" description = "my desc" />
-                </div>
-                <div className="col-md-4">                     
-                <NewsItem title= " my title" description = "my desc" />
-                </div>
-                </div>
-            </div>
-        );
-    }
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+    };
+  }
+  async componentDidMount(){
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apikey=dbe57b028aeb41e285a226a94865f7a7"
+    let data =  await fetch(url)
+    let parsedData = await data.json()
+    this.setState({
+        articles : parsedData.articles
+    })
+  }
+  render() {
+    return (
+      <div className="container my-3">
+        <h2>NewsMonkey-Top Headlines </h2>
+        <div className="row">
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title.slice(0,44) : "title is none"}
+                  description={element.description ? element.description.slice(0,88): " description is null"}
+                  imageUrl={element.urlToImage}
+                  newsUrl = {element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
-
